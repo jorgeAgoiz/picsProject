a78f0e39ea24dacdcd10fcb4fe02821bc7878751
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-03-02 17:26:26 +0100

    Last update today

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  )
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term)
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        {/* Importante entender esto */}
+        {/* pasamos la funcion onSearchSubmit como props */}
+      </div>
+    )
+  }
 }
 
 export default App
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,17 @@ import React from 'react'
 class SearchBar extends React.Component {
   state = { term: '' }
 
+  onFormSubmit = (event) => {
+    event.preventDefault()
+    /* Cada vez que enviamos el formulario llamamos a la funcion onSubmit de App component y le pasamos 
+    el valor del form */
+    this.props.onSubmit(this.state.term)
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
