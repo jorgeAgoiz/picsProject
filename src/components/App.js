31a1104575be9dc2c23dc28adfa133e4c92d7c32
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-03-02 22:31:15 +0100

    Calling APIS with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  state = { images: [] }
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID Tc1mwyHhewoorwHlmsnsXO3qZpn3TUM5HzmIBI0yfv4',
+      },
+    })
+
+    this.setState({ images: response.data.results })
+    console.log(this.state.images)
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
         {/* Importante entender esto */}
         {/* pasamos la funcion onSearchSubmit como props */}
       </div>
