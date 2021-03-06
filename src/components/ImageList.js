cd70700149519c04113846e33683b6278d34f739
jorgeAP <jorgeagoiz@gmail.com>
2021-03-06 07:38:15 +0100

    Show images

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    console.log(props.images);
+    const images = props.images.map(image => {
+        return <img src={image.urls.regular} alt={image.alt_description} />;
+    });
+
+    return <div>{images}</div>;
+};
+
+export default ImageList;
