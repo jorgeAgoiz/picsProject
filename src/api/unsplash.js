f092d9d0e7e35fd92c5cf5a0bfa3b20e961f7309
jorgeAP <jorgeagoiz@gmail.com>
2021-03-06 17:15:38 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID Tc1mwyHhewoorwHlmsnsXO3qZpn3TUM5HzmIBI0yfv4',
+        Authorization: 'Client-ID yourApiKey',
     }
 });
